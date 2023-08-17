eae1bf673b6a4b95bded7c57ba487397067ba4f5
tafufuyo <shunchan0725@gmail.com>
2023-08-18 01:28:07 +0900

    tukareta

---
+++ app/src/main/java/com/example/rpg02/ChapterTest.kt
@@ -0,0 +1,11 @@
+package com.example.rpg02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChapterTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter_test)
+    }
+}
--- app/src/main/java/com/example/rpg02/SetupActivity.kt
+++ app/src/main/java/com/example/rpg02/SetupActivity.kt
@@ -1,11 +1,21 @@
 package com.example.rpg02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
+
+        var nextButton = findViewById<Button>(R.id.nextButton)
+        nextButton.setOnClickListener{
+            val intent = Intent(applicationContext, ChapterTest::class.java)
+            intent.putExtra("playerName",findViewById<EditText>(R.id.name).text)
+            startActivity(intent)
+        }
     }
 }
